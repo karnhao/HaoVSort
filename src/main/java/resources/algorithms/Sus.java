d24997a715f1378ca944593606c90d0d9f2a7c7b
karnhao <karn0888422952@gmail.com>
2022-04-08 15:46:45 +0700

    initial sus sort

---
+++ src/main/java/resources/algorithms/Sus.java
@@ -0,0 +1,11 @@
+package resources.algorithms;
+
+import com.hao.haovsort.sorting.utils.Algorithms;
+
+public class Sus extends Algorithms<Sus> {
+
+    @Override
+    public void sort(Integer[] a) throws InterruptedException {
+        
+    }
+}
