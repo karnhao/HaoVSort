2df2062583fede908d2a7a1512e2621317d87bc9
karnhao <karn0888422952@gmail.com>
2022-05-02 06:37:19 +0700

    add wait algorithm

---
+++ src/main/java/resources/algorithms/Wait.java
@@ -0,0 +1,37 @@
+package resources.algorithms;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.hao.haovsort.sorting.args.InvalidArgsException;
+import com.hao.haovsort.sorting.utils.Algorithms;
+
+import org.bukkit.command.CommandSender;
+
+public class Wait extends Algorithms<Wait> {
+
+    @Override
+    public void sort(Integer[] a) throws InterruptedException {
+        Long delay;
+        try {
+            delay = Long.parseLong(this.getArgs()[0]);
+        } catch (NumberFormatException e) {
+            throw new InvalidArgsException("Cannot format " + getArgs()[0] + " as long : " + e.getMessage());
+        }
+        sleep(delay);
+    }
+
+    @Override
+    protected List<String> onTabComplete(CommandSender sender, String[] args) {
+        if (args.length == 1)
+            return Arrays.asList("<miliseconds>");
+        return null;
+    }
+
+    @Override
+    protected void argsFilter(String[] args) throws InvalidArgsException {
+        if (args.length != 1)
+            throw new InvalidArgsException("Expected 1 argument");
+    }
+
+}
