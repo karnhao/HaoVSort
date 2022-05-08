a7a1d318f70c051d1635f19a9b165f728f10614b
karnhao <karn0888422952@gmail.com>
2022-05-08 10:29:34 +0700

    Add sortdelay command

---
+++ src/main/java/com/hao/haovsort/tabcompleter/SortDelayTab.java
@@ -0,0 +1,22 @@
+package com.hao.haovsort.tabcompleter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.hao.haovsort.utils.PlayerSelector;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+public class SortDelayTab implements TabCompleter {
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 1 && args[0].length() == 0) return Arrays.asList("<new_delay>");
+        if (args.length == 2) return PlayerSelector.getSuggestion(args[1]);
+        return Collections.<String>emptyList();
+    }
+    
+}
