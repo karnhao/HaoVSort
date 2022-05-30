1ed0b5c872461b517a8e87a5c9bb5c86508a0000
karnhao <karn0888422952@gmail.com>
2022-05-30 15:01:51 +0700

    Create piano class

---
+++ src/main/java/resources/algorithms/Piano.java
@@ -0,0 +1,12 @@
+package resources.algorithms;
+
+import com.hao.haovsort.sorting.utils.Algorithms;
+
+public class Piano extends Algorithms<Piano> {
+
+    @Override
+    public void sort(Integer[] a) throws InterruptedException {
+        // TODO Auto-generated method stub
+    }
+    
+}
