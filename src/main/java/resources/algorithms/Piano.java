59f01f71ab066cf062cf59388265a0fbc886dbc0
karnhao <karn0888422952@gmail.com>
2022-05-30 18:46:40 +0700

    fixed

--- src/main/java/resources/algorithms/Piano.java
+++ src/main/java/resources/algorithms/Piano.java
@@ -5,7 +5,7 @@ import com.hao.haovsort.sorting.utils.Algorithms;
 public class Piano extends Algorithms<Piano> {
 
     @Override
-    public void sort(Integer[] a) throws InterruptedException {
+    public void sort(Integer[] a) {
         // TODO Auto-generated method stub
     }
     
