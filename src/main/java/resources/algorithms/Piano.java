9c4aba5873a98e81e91304ffabe2885ae05459f5
karnhao <karn0888422952@gmail.com>
2022-06-18 18:42:25 +0700

    Update pom.xml

--- src/main/java/resources/algorithms/Piano.java
+++ src/main/java/resources/algorithms/Piano.java
@@ -1,12 +1,20 @@
 package resources.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.hao.haovsort.sorting.utils.Algorithms;
+import com.xxmicloxx.NoteBlockAPI.model.Song;
 
 public class Piano extends Algorithms<Piano> {
 
+    private Song song;
+    private int length;
+    private Integer[] old_array;
+    private long tick_delay;
+    private List<com.hao.haovsort.sorting.utils.Sound> sound;
+    
     @Override
     public void sort(Integer[] a) {
-        // TODO Auto-generated method stub
     }
-    
 }
